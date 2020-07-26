4f80da990ff696da4867ac022ed9d342464467d7
YichangKong <50892995+YichangKong@users.noreply.github.com>
2020-07-27 00:21:44 +0800

    The five time commits ,  Complete the  Qrcode pages ,and to build a apk. runing the Android is find,and the time is coding QrcodeView ,but not done.

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(//  what is MateialAPP
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.orange, //主题颜色样本，
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.light  , //深色还是浅色 默认浅色
+//        Color primaryColor, //主色，决定导航栏颜色
+//        Color accentColor, //次级色，决定大多数Widget的颜色，如进度条、开关等。
+//        Color cardColor, //卡片颜色
+//        Color dividerColor, //分割线颜色
+         buttonTheme:  ButtonThemeData(disabledColor: Colors.grey ), //按钮主题
+//        Color cursorColor, //输入框光标颜色
+//        Color dialogBackgroundColor,//对话框背景颜色
+//        String fontFamily, //文字字体
+//        TextTheme textTheme,// 字体主题，包括标题、body等文字样式
+//        IconThemeData iconTheme, // Icon的默认样式
+//        TargetPlatform platform, //指定平台，应用特定平台控件风格
       ),
       title: 'Flutter Bank Demo',
       home: HomeView(),
--- lib/pages/Qrcode.dart
+++ lib/pages/Qrcode.dart
@@ -1,6 +1,7 @@
 //这是QrcodeView类的控制页
 import 'dart:typed_data';
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
 
@@ -9,10 +10,22 @@ class QrcodeView extends StatefulWidget{
   _QrcodeViewState createState() => new _QrcodeViewState();
 }
 
+// 我写个 这个博客 有解释我为什么选择flutter开发app
+//B站的聊天 不可以发连接 客气
+
+//  哈哈哈哈哈
+
+
 class _QrcodeViewState extends State<QrcodeView> {
 
   String barcode = '';
   Uint8List bytes = Uint8List(200);
+
+  TextEditingController _controller;//写入框使用
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   //按钮文字style
   static const TextStyle optionStyle =
@@ -22,33 +35,118 @@ class _QrcodeViewState extends State<QrcodeView> {
   initState() {//what happen?? 初始化状态
     super.initState();
   }
+  //单击弹框
+  Future<void> _showMyDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return Container(
+            child: CupertinoAlertDialog(
+
+              content: SingleChildScrollView(
+                padding: EdgeInsets.all(15),
+
+                child: ListBody(
+                  children: <Widget>[
+                    Text("Why to click me ??? \n Why???",style: TextStyle(fontSize: 28),),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                CupertinoDialogAction(
+                  child: Text("Approve"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                CupertinoDialogAction(
+                  child: Text("Come"),
+                  onPressed: () {},
+                ),
+              ],
+            ));
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
+      appBar: AppBar(
+        leading:  GestureDetector(
+            onTap: (){   _showMyDialog(context);  },
+            child:
+            Padding(padding: EdgeInsets.all(10),child:
+            Column(
+              children: <Widget>[
+                Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.red,
+                  size: 30,
+                ),
+              ],
+            ),)
+        ),
+        backgroundColor: Colors.white,
+      ),
+        body: Container(
+          padding: const EdgeInsets.all(15),//整个容器内填充
+          child: ListView(
             children: <Widget>[
               SizedBox(
-                width: 200,
-                height: 200,
-                child: Image.memory(bytes),
+                child: Container(
+                  height: 250,
+                  color: Colors.grey,
+                  child: Image.memory(bytes),
+                  )
               ),
-              Text('RESULT:  $barcode'),
-              RaisedButton(
-                onPressed: _scan,
-                child: Text(
-                  "Scan",
-                  style: optionStyle,
-                ),
-                color: Colors.black,
+              SizedBox(height: 20),
+              TextField(
+                textAlign: TextAlign.center,
+                controller: _controller,
+                decoration: InputDecoration(  icon: Icon(Icons.text_fields),labelText: '请输入你想要生成的网址',
+                  helperText: '或者文本信息 eg: love', ),
+                onSubmitted: (String value) async {
+              await showDialog<void>(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Thanks!'),
+                    content: Text('You typed "$value".'),
+                    actions: <Widget>[
+                      FlatButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+              TextField(
+                textAlign: TextAlign.center,
               ),
+              Text('扫描 - RESULT:  $barcode',
+              style: TextStyle(color: Colors.black,fontSize: 15),),
+              GestureDetector(
+                onTap: _scan,
+                child: RaisedButton(
+                  child: Text(
+                    "扫一扫",
+                    style: optionStyle,
+                  ),
+                  color: Colors.black,
+                )
+        ),
               RaisedButton(
                 onPressed: _scanPhoto,
-                child: Text(
-                  "Scan Photo",
+                child:
+                Text(
+                  "扫描本地相册",
                   style: optionStyle,
                 ),
                 color: Colors.black,
@@ -56,7 +154,7 @@ class _QrcodeViewState extends State<QrcodeView> {
               RaisedButton(
                 onPressed: _generateBarCode,
                 child: Text(
-                  "Generate Barcode",
+                  "生成文本 - 网址",
                   style: optionStyle,
                 ),
                 color: Colors.black,
@@ -64,11 +162,12 @@ class _QrcodeViewState extends State<QrcodeView> {
               RaisedButton(
                 onPressed: _clearBarCode,
                 child: Text(
-                  "Clean Barcode",
+                  "清除生成QR",
                   style: optionStyle,
                 ),
                 color: Colors.black,
               ),
+
             ],
           ),
         ),
